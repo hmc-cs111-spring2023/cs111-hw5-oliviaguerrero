7e8230d456c4d17836e138a633d15e4331d71242
Olivia Guerrero <112516748+OliveBattle@users.noreply.github.com>
2023-02-21 22:09:54 +0000

    Finished 2c
    (we care about ordering too much to make tuples look in any way pretty)

--- regex/src/main/scala/regex/Regex.scala
+++ regex/src/main/scala/regex/Regex.scala
@@ -11,9 +11,9 @@ trait RegularLanguage()
 case object Empty   extends RegularLanguage
 case object Epsilon extends RegularLanguage
 case class Character(val c: Char) extends RegularLanguage
-case class Union(val langs: (RegularLanguage, RegularLanguage))
+case class Union(val lang1: RegularLanguage, val lang2: RegularLanguage)
                                                 extends RegularLanguage
-case class Concat(val langs: (RegularLanguage, RegularLanguage))
+case class Concat(val lang1: RegularLanguage, val lang2: RegularLanguage)
                                                 extends RegularLanguage
 case class Star(val lang: RegularLanguage)  extends RegularLanguage
 
@@ -28,37 +28,56 @@ case class Star(val lang: RegularLanguage)  extends RegularLanguage
 /** Simplifies a regular language. Implemented Recursively. */
 def simplify(lang: RegularLanguage): RegularLanguage =
   lang match
-    case Union(langs) =>
-      if      langs(0) == Empty then langs(1)
-      else if langs(1) == Empty then langs(0) 
-      else Union(simplify(langs(0)), simplify(langs(1)))
+    case Union(lang1, lang2) =>
+      if      lang1 == Empty then lang2
+      else if lang2 == Empty then lang1 
+      else Union(simplify(lang1), simplify(lang2))
 
-    case Concat(langs) => 
-      if langs(0) == Empty || langs(1) == Empty then Empty
-      else if langs(0) == Epsilon then langs(1)
-      else if langs(1) == Epsilon then langs(0)
-      else Concat(simplify(langs(0)), simplify(langs(1)))
+    case Concat(lang1, lang2) => 
+      if lang1 == Empty || lang2 == Empty then Empty
+      else if lang1 == Epsilon then lang2
+      else if lang2 == Epsilon then lang1
+      else Concat(simplify(lang1), simplify(lang2))
 
-    case Star(lang) =>
-      if      lang == Empty   then  Empty
-      else if lang == Epsilon then Epsilon
-      else Star(simplify(lang))
+    case Star(subLang) =>
+      if      subLang == Empty   then  Empty
+      else if subLang == Epsilon then Epsilon
+      else Star(simplify(subLang))
 
     case _ => lang //For Empty/Epsilon/Char, return yourself
 
 /** A language is nullable if it contains ε */
-def nullable(lang: RegularLanguage): Boolean = ???
+def nullable(lang: RegularLanguage): Boolean = 
+  lang match
+    // If you need to, look recursively.
+    case Union(lang1, lang2)  => nullable(lang1) || nullable (lang2)
+    case Concat(lang1, lang2) => nullable(lang1) && nullable (lang2)
+    // Otherwise, decide based on the current class.
+    case Star(subLang)    => true
+    case Epsilon    => true
+    case _          => false //Both empty and char should return false
 
 /** Computes the derivative of a language, with respect to a character */
-def derivative(l: RegularLanguage)(c: Char): RegularLanguage = ???
+def derivative(l: RegularLanguage)(c: Char): RegularLanguage = 
+  l match
+    case Concat(lang1, lang2) =>
+      val restOfL = Concat(derivative(lang1)(c), lang2)
+      if nullable(lang1) then Union(restOfL, derivative(lang2)(c))
+      else restOfL
+    case Union(lang1, lang2) => Union( derivative(lang1)(c),
+                                       derivative(lang2)(c))
+    case Star(subLang) => Concat(derivative(subLang)(c), Star(subLang))
+    case Character(charC) => if c == charC then Epsilon else Empty
+    case _ => Empty // Both Empty and Epsilon return Empty
+  
 
 /** *****************************************************************************
   * String-matching with regular expressions
   */
 
-/** Given a string s and a language l, returns true if the string matches the
-  * pattern described by l
+/** Given a string s and a language subLang, returns true if the string matches the
+  * pattern described by subLang
   */
-def matches(s: String, l: RegularLanguage): Boolean =
-  if (s.isEmpty) then nullable(l)
-  else matches(s.tail, derivative(l)(s.head))
+def matches(s: String, subLang: RegularLanguage): Boolean =
+  if (s.isEmpty) then nullable(subLang)
+  else matches(s.tail, derivative(subLang)(s.head))
