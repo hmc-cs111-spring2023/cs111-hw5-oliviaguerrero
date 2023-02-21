b68479dd9d88705b539f9473435c6a7994fb5970
Olivia Guerrero <112516748+OliveBattle@users.noreply.github.com>
2023-02-21 21:38:03 +0000

    2b, simplify (tuples is kinda messy)

--- regex/src/main/scala/regex/Regex.scala
+++ regex/src/main/scala/regex/Regex.scala
@@ -11,11 +11,11 @@ trait RegularLanguage()
 case object Empty   extends RegularLanguage
 case object Epsilon extends RegularLanguage
 case class Character(val c: Char) extends RegularLanguage
-case class Union(val left: RegularLanguage, val right: RegularLanguage)
+case class Union(val langs: (RegularLanguage, RegularLanguage))
                                                 extends RegularLanguage
-case class Concat(val first: RegularLanguage, val rest: RegularLanguage)
+case class Concat(val langs: (RegularLanguage, RegularLanguage))
                                                 extends RegularLanguage
-case class Star(val repeater: RegularLanguage)  extends RegularLanguage
+case class Star(val lang: RegularLanguage)  extends RegularLanguage
 
 
 
@@ -25,8 +25,26 @@ case class Star(val repeater: RegularLanguage)  extends RegularLanguage
   * Fill in the function definitions below
   */
 
-/** Simplifies a regular language */
-def simplify(lang: RegularLanguage): RegularLanguage = ???
+/** Simplifies a regular language. Implemented Recursively. */
+def simplify(lang: RegularLanguage): RegularLanguage =
+  lang match
+    case Union(langs) =>
+      if      langs(0) == Empty then langs(1)
+      else if langs(1) == Empty then langs(0) 
+      else Union(simplify(langs(0)), simplify(langs(1)))
+
+    case Concat(langs) => 
+      if langs(0) == Empty || langs(1) == Empty then Empty
+      else if langs(0) == Epsilon then langs(1)
+      else if langs(1) == Epsilon then langs(0)
+      else Concat(simplify(langs(0)), simplify(langs(1)))
+
+    case Star(lang) =>
+      if      lang == Empty   then  Empty
+      else if lang == Epsilon then Epsilon
+      else Star(simplify(lang))
+
+    case _ => lang //For Empty/Epsilon/Char, return yourself
 
 /** A language is nullable if it contains ε */
 def nullable(lang: RegularLanguage): Boolean = ???
